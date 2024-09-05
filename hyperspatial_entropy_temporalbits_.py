09d51d563c43b79a5c1dbe5f7501bb3cacbb2cd5
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2024-09-04 20:21:04 -0400

    git add "First version of the Temporal Nat Qubits"

---
+++ hyperspatial_entropy_temporalbits_.py
@@ -0,0 +1,24 @@
+# Exploration of Entropy in Hyperspatial Vaccum(s)
+
+# Q1: Is The Second Law of Thermodyanmics is mandatory for "superpentaspaces" (such that dimensionalities are a total of five or more spacetime usually with 3 or more spatial dimensions, with at least one, but preferably, three or more time dimensions) or do different laws apply to manifolds and therefore spatial dimension cappers, as well as temporal dimensional cappers?
+import math
+
+#Q2: Is pi, the possibility of safety in measuring the usage of the sun to determine time left before the natural logarithm of of the danger of the night sky of the enemy of the Homo Sapiens Sapiens as the Ancient philosophers Archimedes, Euclid, Socrates, Plato, and Ada Lovelace.
+from math import pi
+# The idea to import all of math may be crucial to the viability of this program and all subsequent programs!
+from  math import log2
+from matplotlib import pyplot
+
+# Step 1: List all of the probabilities of "microstates" or possible outcomes to be more general
+
+probability_list = [1.0, 2.0, 3.0]
+
+# Step 2: Define Information with base 2 modulation to accomodate bits
+
+information = [log2 for p in probability_list]
+
+# Step 3: Define Information with the natural log based modulation to accomodate "nats" or naturual bits.
+
+entropy = len(2*pi*probability_list)
+
+# Q(?):
